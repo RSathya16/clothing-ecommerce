1208d659afea4b3076839952ffacf5e00486f390
R-sathya <113597269+R-sathya@users.noreply.github.com>
2023-09-24 21:07:32 +0530

    Changes for Tablet view and Popup changes for all views

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 console.log("Hiiii>")
     document.getElementById("homenav").addEventListener("click" ,function(){
         console.log("text")
+        document.querySelector(".success").style.display="none";
         document.querySelector(".main-home").style.display="flex";
         document.querySelector(".about").style.display="none";
         document.querySelector(".query").style.display="none";
         document.querySelector(".contactinfo").style.display="none";
         })
     document.getElementById("aboutnav").addEventListener("click" ,function(){
+        document.querySelector(".success").style.display="none";
         document.querySelector(".main-home").style.display="none";
         document.querySelector(".about").style.display="flex";
         document.querySelector(".query").style.display="none";
@@ -25,34 +27,34 @@ console.log("Hiiii>")
     })
     
 function closePopup() {
-    document.querySelector(".popup").style.opacity = 0;
-    document.querySelector(".popup").style.visibility = "hidden";
+    document.querySelector(".popup-box").style.opacity = 0;
+    document.querySelector(".popup-box").style.display = "none";
     document.querySelector(".Parent").style.display = "none";
     
 }
 
 document.getElementById("queryButton").addEventListener("click" ,function(){
     
-    document.querySelector(".popup").style.visibility = "visible" ;
-    document.querySelector(".Parent").style.display = "flex";
-    document.querySelector(".typetext").value =' ';
-    document.querySelector(".popup").style.opacity = 1;
+    document.querySelector(".popup-box").style.display = "block" ;
+    document.querySelector(".Parent").style.display = "block";
+    document.querySelector(".typetext").value ='';
+    document.querySelector(".popup-box").style.opacity = 1;
    
 })
 function message(){
     const typetext=document.getElementById("typetext").value;
     const success=document.getElementById("success");
     const danger=document.getElementById("danger");
-
+    
     if(typetext !== ''){
-        console.log("text")
-        document.querySelector(".popup").style.opacity = 0;
+        console.log("text1")
+        document.querySelector(".popup-box").style.opacity = 0;
         document.querySelector(".danger").style.display="none";
         document.querySelector(".success").style.display="flex";
-        document.querySelector(".popup").style.visibility = "hidden";
+        document.querySelector(".popup-box").style.display = "none";
         document.querySelector(".Parent").style.display = "none";
     }else{
-        console.log("text")
+        console.log("text2")
         document.querySelector(".danger").style.display="flex";
         document.querySelector(".success").style.display="none";
        
@@ -65,6 +67,7 @@ function message(){
 
     })
     document.getElementById("contactnav").addEventListener("click" ,function(){
+        document.querySelector(".success").style.display="none";
         console.log("text")
         document.querySelector(".main-home").style.display="none";
         document.querySelector(".about").style.display="none";
