83e321a9fd273c8edc488638c8a8e1f0181b38b7
R-sathya <113597269+R-sathya@users.noreply.github.com>
2023-09-27 17:36:02 +0530

    The final changes

---
+++ closepopup.js
@@ -0,0 +1,8 @@
+
+const closepopup = (()=>{
+    document.querySelector(".popup").style.opacity = 0;
+    document.querySelector(".popup").style.visibility = "hidden";
+    document.querySelector(".Parent").style.display = "none";
+    
+})
+export default  closepopup 
